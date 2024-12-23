e9ea8f14e225830c70f42ea5af4bf4566a9ebf29
tamkiti132 <harada.a.0907@gmail.com>
2024-12-24 02:32:46 +0900

    グループ内メンバーに対するブロック/ブロック解除機能のテストを実装しました

---
+++ tests/Feature/Http/Livewire/MemberEditTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\Feature\Http\Livewire;
+
+use App\Http\Livewire\MemberEdit;
+use App\Models\Group;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class MemberEditTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // テスト用のロケールを設定
+        app()->setLocale('testing');
+        // テスト用のストレージを設定
+        Storage::fake('public');
+    }
+
+    public function test_blockMember()
+    {
+        // Arrange（準備）
+        // 管理者（ブロックする側のユーザー）
+        $manager = User::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $this->actingAs($manager);
+
+        $group = Group::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $group->user()->attach($manager);
+        $group->userRoles()->attach($manager, ['role' => 10]);
+
+        // ブロックされるユーザー
+        $user = User::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $group->user()->attach($user);
+        $group->userRoles()->attach($user, ['role' => 100]);
+
+
+        // ブロックされるユーザーがブロックされていない状態であることを確認
+        $this->assertDatabaseMissing('block_states', [
+            'user_id' => $user->id,
+            'group_id' => $group->id,
+        ]);
+
+        // セッションにグループIDをセット
+        session()->put('group_id', $group->id);
+
+        // Act（実行） & Assert（検証）        
+        Livewire::test(MemberEdit::class, ['group_id' => $group->id])
+            ->call('blockMember', $user->id);
+
+        // ブロックされるユーザーがブロックされている状態であることを確認
+        $this->assertDatabaseHas('block_states', [
+            'user_id' => $user->id,
+            'group_id' => $group->id,
+        ]);
+    }
+
+    public function test_liftBlockMember()
+    {
+        // Arrange（準備）
+        // 管理者（ブロック解除する側のユーザー）
+        $manager = User::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $this->actingAs($manager);
+
+        $group = Group::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $group->user()->attach($manager);
+        $group->userRoles()->attach($manager, ['role' => 10]);
+
+        // ブロック解除されるユーザー
+        $user = User::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $group->user()->attach($user);
+        $group->userRoles()->attach($user, ['role' => 100]);
+
+        // ユーザーをブロック状態にしておく
+        $user->blockedGroup()->attach($group->id);
+
+
+        // ブロックされるユーザーがブロックされている状態であることを確認
+        $this->assertDatabaseHas('block_states', [
+            'user_id' => $user->id,
+            'group_id' => $group->id,
+        ]);
+
+        // セッションにグループIDをセット
+        session()->put('group_id', $group->id);
+
+        // Act（実行） & Assert（検証）
+        Livewire::test(MemberEdit::class, ['group_id' => $group->id])
+            ->call('liftBlockMember', $user->id);
+
+        // ブロックされるユーザーがブロックされていない状態であることを確認
+        $this->assertDatabaseMissing('block_states', [
+            'user_id' => $user->id,
+            'group_id' => $group->id,
+        ]);
+    }
+}
