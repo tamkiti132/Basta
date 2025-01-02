5e9f9745c4af29c01cd24cb4fab6cafa1dc5c4ea
tamkiti132 <harada.a.0907@gmail.com>
2025-01-03 00:31:26 +0900

    test: グループトップページでの退会処理に対するテストを実装

--- tests/Feature/Http/Livewire/QuitGroupFormTest.php
+++ tests/Feature/Http/Livewire/QuitGroupFormTest.php
@@ -66,4 +66,53 @@ class QuitGroupFormTest extends TestCase
             'group_id' => $group->id,
         ]);
     }
+
+    public function test_validation_失敗_quitGroup()
+    {
+        // Arrange（準備）
+        // 管理者を追加
+        $manager = User::factory()->create([
+            'suspension_state' => 0,
+        ]);
+
+        $group = Group::factory()->create([
+            'suspension_state' => 0,
+        ]);
+        $group->userRoles()->attach($manager, ['role' => 10]);
+
+        // メンバーを追加
+        $password = 'member-password';
+        $member = User::factory()->create([
+            'suspension_state' => 0,
+            'password' => Hash::make($password),
+        ]);
+
+        $group->userRoles()->attach($member, ['role' => 100]);
+
+        // メンバーとしてログイン
+        $this->actingAs($member);
+
+        // Act（実行） & Assert（検証）
+        // グループトップページにアクセスして、QuitGroupFormコンポーネントが存在するか確認
+        $this->get('/group/top/' . $group->id)
+            ->assertSeeLivewire(QuitGroupForm::class);
+
+        // フォームを送信して、メンバーが退会できるか確認
+        // passwordのバリデーション
+        Livewire::test(QuitGroupForm::class)
+            ->set('password', '')
+            ->call('quitGroup')
+            ->assertHasErrors(['password' => 'required']);
+
+        Livewire::test(QuitGroupForm::class)
+            ->set('password', 'aaaa')
+            ->call('quitGroup')
+            ->assertHasErrors(['password' => 'current_password']);
+
+        // メンバーが退会したか確認（rolesテーブルから削除されていないか）
+        $this->assertDatabaseHas('roles', [
+            'user_id' => $member->id,
+            'group_id' => $group->id,
+        ]);
+    }
 }
