ce8b62437105dc7b263728248921e27b6de756a5
tamkiti132 <harada.a.0907@gmail.com>
2025-01-02 19:25:34 +0900

    test: setUpメソッドを記述し忘れているテストファイルに対して、setUpメソッドを追加しました

--- tests/Feature/vendor/laravel/jetstream/src/Http/Livewire/UpdateProfileInformationFormTest.php
+++ tests/Feature/vendor/laravel/jetstream/src/Http/Livewire/UpdateProfileInformationFormTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature\vendor\laravel\jetstream\src\Http\Livewire;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
