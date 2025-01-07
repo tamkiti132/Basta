676fc63d7eefce8651c10e15983a2ea816b61ad1
tamkiti132 <harada.a.0907@gmail.com>
2025-01-07 19:11:51 +0900

    refactor: グループ招待メール送信処理周りのリファクタリングと、それに関するテストを実装しました。

--- tests/Feature/Http/Livewire/GroupEditTest.php
+++ tests/Feature/Http/Livewire/GroupEditTest.php
@@ -3,10 +3,12 @@
 namespace Tests\Feature\Http\Livewire;
 
 use App\Http\Livewire\GroupEdit;
+use App\Mail\InviteMail;
 use App\Models\Group;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Livewire;
@@ -98,7 +100,7 @@ class GroupEditTest extends TestCase
 
 
 
-    public function test_バリデーション_失敗_updateGroupInfo()
+    public function test_validation_失敗_updateGroupInfo()
     {
         // Arrange（準備）
         $user = User::factory()->create([
@@ -168,5 +170,91 @@ class GroupEditTest extends TestCase
             'name' => '更新前のグループ名',
             'introduction' => '更新前のグループ紹介文',
         ]);
+    }
+
+    public function test_sendInviteToGroupMail_check_send_mail()
+    {
+        // Arrange（準備）
+        // 招待メールを送信するユーザー
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'suspension_state' => 0,
+        ]);
+        $this->actingAs($user);
+
+        $group = Group::factory()->create([
+            'name' => 'テストグループ名',
+            'introduction' => 'テストグループ紹介文',
+            'suspension_state' => 0,
+        ]);
+        $group->userRoles()->attach($user, ['role' => 10]);
+
+        // 招待されるユーザー
+        $target_user = User::factory()->create([
+            'email' => 'target@example.com',
+            'suspension_state' => 0,
+        ]);
+
+        Mail::fake();
+
+        // Act（実行）  & Assert（検証）
+        Livewire::test(GroupEdit::class, ['group_id' => $group->id])
+            ->set('email', $target_user->email)
+            ->call('sendInviteToGroupMail')
+            ->assertRedirect('group/group_edit/' . $group->id);
+
+        // 送信元 ・ 送信先のメールアドレスは app/Mail/InviteMail.php に記載
+        Mail::assertSent(InviteMail::class, function ($mail) {
+            return $mail->hasTo('target@example.com') &&
+                $mail->hasSubject('Basta グループ招待');
+        });
+    }
+
+    public function test_validation_失敗_sendInviteToGroupMail()
+    {
+        // Arrange（準備）
+        // 招待メールを送信するユーザー
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'suspension_state' => 0,
+        ]);
+        $this->actingAs($user);
+
+        $group = Group::factory()->create([
+            'name' => 'テストグループ名',
+            'introduction' => 'テストグループ紹介文',
+            'suspension_state' => 0,
+        ]);
+        $group->userRoles()->attach($user, ['role' => 10]);
+
+        // 招待されるユーザー
+        $target_user = User::factory()->create([
+            'email' => 'target@example.com',
+            'suspension_state' => 0,
+        ]);
+
+        Mail::fake();
+
+        // Act（実行）  & Assert（検証）
+        // emailのバリデーション
+        Livewire::test(GroupEdit::class, ['group_id' => $group->id])
+            ->set('email', '')
+            ->call('sendInviteToGroupMail')
+            ->assertHasErrors(['email' => 'required']);
+
+        Livewire::test(GroupEdit::class, ['group_id' => $group->id])
+            ->set('email', 123)
+            ->call('sendInviteToGroupMail')
+            ->assertHasErrors(['email' => 'string']);
+
+        Livewire::test(GroupEdit::class, ['group_id' => $group->id])
+            ->set('email', 'not_email')
+            ->call('sendInviteToGroupMail')
+            ->assertHasErrors(['email' => 'email']);
+
+        Livewire::test(GroupEdit::class, ['group_id' => $group->id])
+            ->set('email', 'not_exists@example.com')
+            ->call('sendInviteToGroupMail')
+            ->assertHasErrors(['email' => 'exists:users,email']);
     }
 }
