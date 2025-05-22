0feed0bc1e5f9ded2cdf731ae8061e44a3adba68
tamkiti132 <harada.a.0907@gmail.com>
2025-05-22 22:14:28 +0900

    fix: メール表示部分のコードを調整
    本当は、メールのレイアウトが崩れるのを直したかったが、これはGmailのdeepl翻訳機能によるバグのようなものだと思うので、今回は気にしないことにした

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
-<img src="{{ asset('images/logo.png') }}" alt="LOGO" style="max-width: 100px;">
+  
 </x-mail::header>
 </x-slot:header>
 
