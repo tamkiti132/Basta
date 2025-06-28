3f2d207e5b65209a7cddaef90ec94cc8e3080140
tamkiti132 <harada.a.0907@gmail.com>
2025-06-28 19:42:03 +0900

    fix: ログイン画面と新規登録画面のレスポンシブ用のUIを調整

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -2,5 +2,5 @@
     @can('admin-top'){{ route('admin.user_top') }}
     @else {{ route('index') }}
     @endcan">
-    <img src="{{ asset('images/logo.png') }}" alt="">
+    <img src="{{ asset('images/logo.png') }}" alt="" class="mx-auto w-28 h-28 sm:w-40 sm:h-40">
 </a>
